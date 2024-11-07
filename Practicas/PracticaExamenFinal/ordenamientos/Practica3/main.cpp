bc7dea0c7ad7289990a423f636ed5eacca9f98f6
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-11-06 18:01:01 -0600

    Add Shaker Sort algorithm implementation and update VSCode settings

--- Practicas/PracticaExamenFinal/ordenamientos/Practica3/main.cpp
+++ Practicas/PracticaExamenFinal/ordenamientos/Practica3/main.cpp
@@ -1,61 +1,69 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;    
+// Implementación del algoritmo Shaker Sort
 void shakerSort(vector<int>& numbers) {
-
     bool swapped = true;
-
     int start = 0;
-
     int end = numbers.size() - 1;
 
-
-
     while (swapped) {
-
         swapped = false;
 
-        for (int i = 0; i < end; i++) {
-
+        // Primera pasada, de izquierda a derecha
+        for (int i = start; i < end; i++) {
             if (numbers[i] > numbers[i + 1]) {
-
                 int temp = numbers[i];
-
                 numbers[i] = numbers[i + 1];
-
                 numbers[i + 1] = temp;
-
                 swapped = true;
-
             }
-
         }
 
-
-
+        // Si no hubo intercambios, el array ya está ordenado
         if (!swapped) break;
 
+        // Ajusta el final de la pasada y resetea el indicador de cambio
         end--;
+        swapped = false;
 
-        swapped = false; 
-
-
-
+        // Segunda pasada, de derecha a izquierda
         for (int j = end; j > start; j--) {
-
             if (numbers[j] < numbers[j - 1]) {
-
                 int temp = numbers[j];
-
-                numbers[j] = numbers[j-1];
-
+                numbers[j] = numbers[j - 1];
                 numbers[j - 1] = temp;
-
                 swapped = true;
-
             }
-
         }
 
+        // Ajusta el inicio de la siguiente pasada
         start++;
-
     }
+}
 
+// Función para mostrar el contenido del vector
+void mostrarVector(const vector<int>& array) {
+    for (int num : array) {
+        cout << num << " ";
+    }
+    cout << std::endl;
+}
+
+int main() {
+    // Ejemplo de uso del método Shaker Sort
+    vector<int> numbers = {29, 10, 14, 37, 13, 23, 25, 30};
+
+    cout << "Array original:" << endl;
+    mostrarVector(numbers);
+
+    // Llamada al método Shaker Sort
+    shakerSort(numbers);
+
+    std::cout << "Array ordenado:" << endl;
+    mostrarVector(numbers);
+
+    return 0;
 }
